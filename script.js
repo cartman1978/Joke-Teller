3f14af974f87a9eb4123cc7b16a0447f6a621e0a
cartman1978 <cartman_7802@yahoo.ie>
2021-03-27 11:39:23 +0000

    SDK modified

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const VoiceRSS={speech(e){this._validate(e),this._request(e)},_validate(e){if(!e
 
 
 
+
 function test() {
      VoiceRSS.speech({
             key: '20d262183b1b474381fe2a3b78ce5b84',
